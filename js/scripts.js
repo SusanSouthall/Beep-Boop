fcf7cd153c788c7c86cba9ef183e84865675e884
Susan Southall <sue@proteanaegis.com>
2018-03-30 13:16:11 -0700

    Add HTML text, jQuery, js logic to return a number to user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-ECHO is on.
+// NOTE: Begins Business logic.
+
+function convert(number) {
+
+var responses = ["Beep!", "Boop!", "I'm sorry Dave, I can't do that."];
+
+var numbers = [];
+// var newNumber = $("input#userInput").val();
+var splitNumbers = number.toString().split("").map(function(number){
+	return parseInt(number);
+});
+ numbers.push(splitNumbers);
+ // numbers = numbers.toString();
+//
+//
+//
+//   if(number%3 === 0) {
+//     alert(responses[2]);
+//   }else if (number)
+  return numbers;
+}
+
+
+
+// NOTE: Begins User Interface.
+
+$(document).ready(function(){
+  $("form#convertor").submit(function(event){
+    event.preventDefault();
+    var userNumber =  $("input#userInput").val();
+    debugger;
+    // convert(userNumber);
+    // var name = $("input#userInput").val();
+    //
+    // var numbers = [];
+    //
+    //
+    var userOutput = convert(userNumber);
+    $(".userOutput").text(userOutput);
+
+  });
+});
